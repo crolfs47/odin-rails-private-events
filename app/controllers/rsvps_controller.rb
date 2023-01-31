a8c24ddf01da1d1f17a7ca5b147c9cf998b94d97
Cathy Rolfs <crolfs47@gmail.com>
2023-01-31 17:16:28 -0600

    Clean up unnecessary code, hide fields in rsvp form in  event/show view

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,21 +1,13 @@
 class RsvpsController < ApplicationController
 
-def new
-
-end
-
 def create
   @rsvp = current_user.rsvps.build(attended_event_id: params[:attended_event_id])
-  
+
   if @rsvp.save
     redirect_to Event.find(@rsvp.attended_event_id)
   else
     
   end
-end
-
-def show
-  @rsvp
 end
 
 private

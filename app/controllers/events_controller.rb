332133841df8cb703178193498f5aebfe06864e5
Cathy Rolfs <crolfs47@gmail.com>
2023-01-31 09:55:46 -0600

    Add #create action to create new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new(post_params)
+  end
+
+  def create
+    @event = current_user.created_events.build(post_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:event).permit(:title, :location, :date, :start_time, :end_time)
+  end
 end

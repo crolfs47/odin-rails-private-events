2f1ce4317757e0c5b43d9cc11cd6b54578bfc8d9
Cathy Rolfs <crolfs47@gmail.com>
2023-01-31 11:30:51 -0600

    Add user #show action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-    @user_events = current_user.created_events.all
+    @user = User.find(params[:id])
+    @user_events = @user.created_events.all
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:email, :name)
   end
 end

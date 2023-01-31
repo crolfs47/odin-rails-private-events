5df30281d394a295239f6b2cdb1991636a3952fe
Cathy Rolfs <crolfs47@gmail.com>
2023-01-31 17:14:16 -0600

    Add #create action in Rsvps Controller, add form to event #show view to post to Rsvp

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   
   belongs_to :creator, class_name: 'User'
   has_many :attendees, through: :rsvps
-  has_many :rsvps, foreign_key: attended_event_id
+  has_many :rsvps, foreign_key: :attended_event_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
   has_many :attended_events, through: :rsvps
-  has_many :rsvps, foreign_key: :attended_id
+  has_many :rsvps, foreign_key: :attendee_id
 end

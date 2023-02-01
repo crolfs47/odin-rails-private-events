715b633399fe80607b15989768e84af45bc79676
Cathy Rolfs <crolfs47@gmail.com>
2023-02-01 12:06:34 -0600

    Add class methods to Event model to separate and order past and upcoming events on Event #index view

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :rsvps, foreign_key: :attended_event_id
   has_many :attendees, through: :rsvps
+
+  def self.past
+    where('date < ?', Date.today).order('date DESC')
+  end
+
+  def self.upcoming
+    where('date > ?', Date.today).order('date ASC')
+  end
 end
